f5e8ebd0e4c251885a114046c5fe925608b1354a
Ismail Gaddipati <Ismail@IMAC.fios-router.home>
2020-06-15 23:35:01 -0400

    update database

--- server.js
+++ server.js
@@ -9,13 +9,12 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 const register = require('./controllers/register');
 
-process.env.NODE_TLS_REJECT_UNATHORIZED = 0
 
 const db = knex({
 	client: 'pg',
 	connection:{
-		connectionString:process.env.DATABASE_URL,
-		ssl: true
+		connectionString: process.env.DATABASE_URL,
+		ssl: true,
 	}
 })
 
