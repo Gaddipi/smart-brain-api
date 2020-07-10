7d384194ca9ad6e34072fd56a3af728472a35ce7
Ismail Gaddipati <Ismail@imac.myfiosgateway.com>
2020-07-09 22:59:03 -0400

    testing heroku connection

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {res.send('it is working!')})
+app.get("/", (req, res) => {res.send('it is working!!!!!!!!')})
 app.post("/signin", signIn.handlesignIn(db, bcrypt));
 app.post("/register", (req, res) => {
 	register.handleRegister(req, res, db, bcrypt)});
