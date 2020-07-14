5bcbf61db4b9c10c30f923aaed54f63a38f3cc29
Ismail Gaddipati <Ismail@IMAC.fios-router.home>
2020-07-14 16:47:43 -0400

    testing herokou connection

--- controllers/register.js
+++ controllers/register.js
@@ -5,39 +5,34 @@ const handleRegister = (req, res, db, bcrypt) => {
 	}
 	const hash = bcrypt.hashSync(password);
 
-	db('login').insert({hash:hash, email:email})
+	
+	// db('login').insert({hash:hash, email:email})
 
-// db.transaction(trx => {
-//       trx.insert({
-//         hash: hash,
-//         email: email
-//       })
-//       .into('login')
-//     //   .returning('email')
-//     //   .then(loginEmail => {
-//     //     return trx('users')
-//     //       .returning('*')
-//     //       .insert({
-//     //         email: loginEmail[0],
-//     //         name: name,
-//     //         joined: new Date()
-//     //       })
-//     //       .then(user => {
-//     //         res.json(user[0]);
-//     //       })
-//     //   })
-//       .then(trx.commit)
-//       .catch(trx.rollback)
-//     })
-//     .catch(err => res.status(400).json('unable to register'))
+db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json(db))
 }
 module.exports = {
 	handleRegister: handleRegister,
 };
 
-
-
-
-module.exports = {
-	handleRegister: handleRegister,
-};
